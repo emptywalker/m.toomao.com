c7f94c3af04d3048f99672550f0a107e517ae724
xuyouhong <1647064983@qq.com>
2016-08-16 13:59:38 +0800

    this is add message

---
+++ HomePage/JS/HomePage.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 16/8/15.
+ */
+
+// document.onload (function () {
+//
+//     var aLabels = document.getElementsByTagName('a');
+//     // .onclick(function (e) {
+//     //     alert(e.innerText);
+//     // });
+// });
+
+var aLabels = document.getElementsByTagName('a');
