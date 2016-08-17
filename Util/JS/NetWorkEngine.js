b1d72ccdc3b9b4f28c00131f31de70ce34b823e8
xuyouhong <1647064983@qq.com>
2016-08-17 15:22:11 +0800

    add News

---
+++ Util/JS/NetWorkEngine.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Administrator on 16/8/16.
+ */
+
+
+var URLBase = 'https://dev-api.toomao.com';//'https://api.toomao.com';//
+
+
+//发送网络请求
+function ajax(options) {
+    options = options || {};
+    options.type = (options.type || "GET").toUpperCase();
+    options.dataType = options.dataType || "json";
+    var params = JSON.stringify( options.data);
+
+    //创建 - 非IE6 - 第一步
+    if (window.XMLHttpRequest) {
+        var xhr = new XMLHttpRequest();
+    } else {
+        //IE6及其以下版本浏览器
+        var xhr = new ActiveXObject('Microsoft.XMLHTTP');
+    }
+
+    //接收 - 第三步
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            var status = xhr.status;
+            if (status >= 200 && status < 300) {
+                options.success && options.success(xhr.responseText, xhr.response);
+            } else {
+                options.fail && options.fail(status);
+            }
+        }
+    }
+    //连接 和 发送 - 第二步
+    xhr.open(options.type, options.url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(params);
+}
