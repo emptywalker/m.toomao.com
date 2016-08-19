5bc8a9b1113102dcf35a44fa4796572b548b0541
xuyouhong <1647064983@qq.com>
2016-08-19 13:46:10 +0800

    add home page scroll

--- HomePage/JS/HomePage.js
+++ HomePage/JS/HomePage.js
@@ -2,12 +2,22 @@
  * Created by Administrator on 16/8/15.
  */
 
-// document.onload (function () {
-//
-//     var aLabels = document.getElementsByTagName('a');
-//     // .onclick(function (e) {
-//     //     alert(e.innerText);
-//     // });
-// });
+window.onload = function () {
+    var screenWidth = window.screen.availWidth;
+    var screenHeight = window.screen.availHeight - 67;
 
-var aLabels = document.getElementsByTagName('a');
+    var warp = document.querySelector('#warp');
+    var scrollBody = document.querySelector('#scrollBoby');
+    var items = document.querySelectorAll('.item');
+
+    scrollBody.style.width = 6 * screenWidth + 'px';
+    warp.style.height = screenHeight + 'px';
+
+    for (var i = 0; i < items.length; i ++){
+        var item = items[i];
+        item.style.width = screenWidth + 'px';
+    }
+
+    console.log(warp.offsetWidth);
+
+}
