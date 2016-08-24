576e1c0bda0e438a3f4992ba031c1b7f0d1ec197
xuyouhong <1647064983@qq.com>
2016-08-24 15:50:42 +0800

    修改导航位置

--- BusinessEnter/JS/BusinessEnter.js
+++ BusinessEnter/JS/BusinessEnter.js
@@ -1,7 +1,11 @@
 /**
  * Created by Administrator on 16/8/17.
  */
+
+
+//导航定位 将当前标签栏移到屏幕内
+document.querySelector('.hideA').click();
 window.onload = function () {
     //导航定位 将当前标签栏移到屏幕内
-    document.querySelector('.hideA').click();
+    // document.querySelector('.hideA').click();
 }
